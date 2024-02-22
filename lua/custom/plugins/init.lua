0d65ae08fd717f2c3aca23af3cdf23af4447d81b
Sascha Fricke <fricke@cg.cs.tu-bs.de>
2024-02-22 12:04:31 +0100

    img-clip

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,6 +13,20 @@ return {
     'tikhomirov/vim-glsl',
     'sbdchd/neoformat',
     'tpope/vim-surround',
+
+    {
+        "HakonHarnes/img-clip.nvim",
+        event = "BufEnter",
+        opts = {
+            -- add options here
+            -- or leave it empty to use the default settings
+        },
+        keys = {
+            -- suggested keymap
+            { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+        },
+    },
+
     {
         'mfussenegger/nvim-dap', config = function ()
             local dap = require('dap')
